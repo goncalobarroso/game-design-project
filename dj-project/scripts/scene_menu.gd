16ba30ed867909a405710c708f255589ea404475
goncalobarroso <goncalobarroso201@gmail.com>
2025-06-05 18:03:37 +0100

    Connected home scene and work scene

--- dj-project/scripts/scene_menu.gd
+++ dj-project/scripts/scene_menu.gd
@@ -23,7 +23,11 @@ func hide_menu():
 func _on_yes_button_pressed() -> void:
 	$AnimationPlayer.play_backwards("blur")
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Scenes/street.tscn")
+	if get_tree().current_scene.name == "home":
+		get_tree().change_scene_to_file("res://Scenes/work.tscn")
+	elif get_tree().current_scene.name == "work":
+		get_tree().change_scene_to_file("res://Scenes/home.tscn")
+	
 
 func _on_no_button_pressed() -> void:
 	$AnimationPlayer.play_backwards("blur")
